00372f7616d9752ad9964f2d6ced2a050f4602de
Yusuf Firmansyah <yusuffirmansyamh@gmail.com>
2024-04-13 07:28:48 +0700

    feat: improve-logic-andpoint-/hadist

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const http = require("node:http");
 const mysql = require("mysql2");
 const express = require("express");
 const cors = require("cors");
@@ -11,7 +10,7 @@ app.use(
 );
 
 // Connect Database
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
@@ -19,31 +18,38 @@ const db = mysql.createConnection({
 });
 
 app.get("/hadist", (req, res) => {
-  const sql = "SELECT * FROM tb_hadist";
+  const sql = `SELECT tb_hadist_content.id,
+    tb_hadist_content.content, tb_hadist_content.id_hadist,
+    tb_hadist.name, tb_hadist.value FROM tb_hadist_content INNER JOIN tb_hadist ON tb_hadist_content.id_hadist = tb_hadist.id`;
+
   db.query(sql, (err, data) => {
-    if (err) return res.json("Error");
+    if (err) {
+      console.error(err);
+      return res.status(500).json("Error");
+    }
 
-    // Ubah nilai content menjadi array langsung
-    const parsedData = data.map((item) => {
-      console.log(item);
-      return {
-        ...item,
-        content:
-          item.content !== null
-            ? [
-                {
-                  data: item.content,
-                },
-              ]
-            : null,
-      };
-    });
+    // Memproses data untuk mengelompokkan konten (content) dengan id_hadist yang sama pada tb_hadist
+    const groupedData = data.reduce((acc, curr) => {
+      if (!acc[curr.id_hadist]) {
+        acc[curr.id_hadist] = {
+          id: curr.id_hadist,
+          name: curr.name,
+          value: curr.value,
+          content: [],
+        };
+      }
+      acc[curr.id_hadist].content.push(curr.content); // Memasukkan nilai content dari setiap baris data ke dalam array content di objek yang sesuai dengan id_hadist.
+      return acc;
+    }, {});
 
-    return res.json(parsedData);
+    // Mengubah objek menjadi array
+    const result = Object.values(groupedData);
+
+    return res.json(result);
   });
 });
 
-// starts a simple http server locally on port 3000
+// starts a simple http server locally on port 1212
 app.listen(1212, () => {
   console.log("Listening on localhost:1212");
 });
